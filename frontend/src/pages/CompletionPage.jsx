05776047df6a2ea29e23638bbcd3f188e9981d1a
Anton Shubin <antshubin023@gmail.com>
2026-03-18 20:49:30 +0300

    Pre-final fronted version for Web

--- frontend/src/pages/CompletionPage.jsx
+++ frontend/src/pages/CompletionPage.jsx
@@ -34,6 +34,7 @@ export default function CompletionPage() {
           Спасибо за участие!
         </h1>
 
+        {/*
         <p style={{ color: 'var(--color-text-muted)', lineHeight: 1.8, marginBottom: '32px' }}>
           Вы успешно завершили исследование. Ваши ответы очень помогут нам
           улучшить качество алгоритмов симуляции.
@@ -83,6 +84,7 @@ export default function CompletionPage() {
         >
           Начать новую сессию
         </button>
+        */}
       </div>
     </div>
   )
